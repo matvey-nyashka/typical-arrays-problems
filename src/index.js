8ec540787d31bacbf44be9353d0739b10921e400
matvey-nyashka <matvej.gnedin.10@mail.ru>
2021-03-12 20:59:51 +0300

    feat: added min, max, avg function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+  return (array && array.length>0) ? Math.min.apply(null, array) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return (array && array.length>0) ? Math.max.apply(null, array) : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return (array && array.length>0) ? array.reduce((sum, current) => sum + current, 0)/array.length : 0;
 }
